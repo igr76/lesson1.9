c90bbf3e8d48955e4b4287663af796ad4d68d64c
igr1 <dfrgth>
2022-09-04 22:47:56 +1000

    правка

---
+++ Author.java
@@ -0,0 +1,15 @@
+public class Author {
+     private String authorName1;
+    Author (String name) {
+        this.authorName1 = name;
+    }
+
+    public String getAuthorName() {
+        return authorName1;
+    }
+
+    public void setAuthorName(String name) {
+        this.authorName1 = name;
+    }
+
+}
---
+++ Book.java
@@ -0,0 +1,26 @@
+public class Book {
+    private  String authorName;
+    private int publishingYear;
+
+
+    Book (String name, int year) {
+        this.authorName = name;
+        this.publishingYear = year;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setAuthorName(String name) {
+        this.authorName = name;
+    }
+    public void setPublishingYear(int name) {
+        this.publishingYear = name;
+    }
+
+}
